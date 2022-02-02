8cba3e37c35dbcdc32e392adcae1aa678ac9860f
Thomas Hall <thall1961@gmail.com>
2022-02-02 08:08:11 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        WRDLE
       </header>
+      <div className="row">
+        <div className="col-2 p-5 bg-light">
+          <input type="text" />
+        </div>
+        <div className="col-2 p-5 bg-light">
+          <input type="text" />
+        </div>
+        <div className="col-2 p-5 bg-light">
+          <input type="text" />
+        </div>
+        <div className="col-2 p-5 bg-light">
+          <input type="text" />
+        </div>
+        <div className="col-2 p-5 bg-light">
+          <input type="text" />
+        </div>
+      </div>
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
+      <div className="row"></div>
     </div>
   );
 }
