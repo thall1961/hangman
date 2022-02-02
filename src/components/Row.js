84a9eaf970c48d5913c7eed34cd4a0c022ba699e
Thomas Hall <thall1961@gmail.com>
2022-02-02 10:06:17 -0700

    stuff

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,19 +4,19 @@ export default () => {
   return (
     <div className="row">
       <div className="col-2 p-5 bg-light mx-1">
-        <input type="text" />
+        <input type="text" minLength="1" maxLength="1" pattern="[A-Za-z]" style={{textTransform: 'uppercase', width: '50px', fontSize: '3rem'}} />
       </div>
       <div className="col-2 p-5 bg-light">
-        <input type="text" />
+        <input type="text" minLength="1" maxLength="1" pattern="[A-Za-z]" style={{textTransform: 'uppercase', width: '50px', fontSize: '3rem'}} />
       </div>
       <div className="col-2 p-5 bg-light">
-        <input type="text" />
+        <input type="text" minLength="1" maxLength="1" pattern="[A-Za-z]" style={{textTransform: 'uppercase', width: '50px', fontSize: '3rem'}} />
       </div>
       <div className="col-2 p-5 bg-light">
-        <input type="text" />
+        <input type="text" minLength="1" maxLength="1" pattern="[A-Za-z]" style={{textTransform: 'uppercase', width: '50px', fontSize: '3rem'}} />
       </div>
       <div className="col-2 p-5 bg-light">
-        <input type="text" />
+        <input type="text" minLength="1" maxLength="1" pattern="[A-Za-z]" style={{textTransform: 'uppercase', width: '50px', fontSize: '3rem'}} />
       </div>
     </div>
   );
