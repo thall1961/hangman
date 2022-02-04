7a2c1a4a555a6f33f25fba3818693333c9ae1d85
Thomas Hall <thall1961@gmail.com>
2022-02-03 21:05:55 -0700

    for phone

--- src/App.js
+++ src/App.js
@@ -1,67 +1,65 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Grid from "./components/Grid";
 import Keyboard from "./components/Keyboard";
 import words from "./data/words";
 
 function App() {
-  let [gameOver, setGameOver] = useState(false);
-  let [row, setRow] = useState(0);
-  let [col, setCol] = useState(0);
-  let [guesses, setGuesses] = useState(
-    Array(6)
-      .fill(" ")
-      .map(() => new Array(5).fill(" "))
-  );
-  const answer = "steam";
+    let [gameOver, setGameOver] = useState(false);
+    let [row, setRow] = useState(0);
+    let [col, setCol] = useState(0);
+    let [guesses, setGuesses] = useState(
+        Array(6)
+            .fill(" ")
+            .map(() => new Array(5).fill(" "))
+    );
+    const answer = "steam";
 
-  const updateGuesses = (guess) => {
-    if (col < 5) {
-      guesses[row][col] = guess;
-      setGuesses(guesses);
-      setCol(col + 1);
-    }
-  };
+    const updateGuesses = (guess) => {
+        if (col < 5) {
+            guesses[row][col] = guess;
+            setGuesses(guesses);
+            setCol(col + 1);
+        }
+    };
 
-  const deleteLetter = () => {
-    if (col > 0 && !gameOver) {
-      guesses[row][col - 1] = " ";
-      setCol(col - 1);
-    }
-  };
+    const deleteLetter = () => {
+        if (col > 0 && !gameOver) {
+            guesses[row][col - 1] = " ";
+            setCol(col - 1);
+        }
+    };
 
-  const nextGuess = () => {
-    if (col < 5) {
-      alert("keep guessing");
-      return;
-    }
-    const guessStr = guesses[row]
-      .reduce((prev, cur) => prev.concat(cur), [])
-      .join("");
-    if (!words.includes(guessStr)) {
-      alert("That word is not in our word bank.");
-      return;
-    }
-    if (guessStr === answer) {
-      alert("You did it!");
-      setGameOver(true);
-      return;
-    }
-    setRow(row + 1);
-    setCol(0);
-  };
+    const nextGuess = () => {
+        if (col < 5) {
+            alert("keep guessing");
+            return;
+        }
+        const guessStr = guesses[row].reduce((prev, cur) => prev.concat(cur), []).join("");
+        if (!words.includes(guessStr)) {
+            alert("That word is not in our word bank.");
+            return;
+        }
+        if (guessStr === answer) {
+            alert("You did it!");
+            setGameOver(true);
+            return;
+        }
+        setRow(row + 1);
+        setCol(0);
+    };
 
-  return (
-    <div className="App">
-      <header className="App-header">WRDLE</header>
-      <Grid guesses={guesses} row={row} col={col} answer={answer} />
-      <Keyboard
-        guessLetter={updateGuesses}
-        deleteLetter={deleteLetter}
-        nextGuess={nextGuess}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">WRDLE</header>
+            <Grid guesses={guesses} row={row} col={col} answer={answer}/>
+            <Keyboard
+                guessLetter={updateGuesses}
+                deleteLetter={deleteLetter}
+                nextGuess={nextGuess}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,61 +1,73 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const Ltr = styled.div`
-  width: 35px !important;
-  height: 35px !important;
+  width: 14px;
+  height: 35px;
   display: flex;
   justify-content: center;
   align-items: center;
   transition: all 0.3s;
+  
+  @media (min-width: 375px) {
+    width: 18px
+  }
 
+  @media (min-width: 425px) {
+    width: 23px
+  }
+
+  @media (min-width: 768px) {
+    width: 35px
+  }
+  
   &:hover {
     cursor: pointer;
   }
 `;
 
 function Letter(props) {
-  let [pressed, setPressed] = useState(false);
+    let [pressed, setPressed] = useState(false);
 
-  const keyPress = () => {
-    setPressed(!pressed);
-  };
+    const keyPress = () => {
+        setPressed(!pressed);
+    };
 
-  const mouseLeft = () => {
-    setPressed(false);
-  };
+    const mouseLeft = () => {
+        setPressed(false);
+    };
 
-  const handleClick = () => {
-    if (props.letter === "❌") {
-      props.deleteLetter();
-    } else if (props.letter === "✅") {
-      props.nextGuess();
-    } else {
-      props.guessLetter(props.letter);
-    }
-  };
+    const handleClick = () => {
+        if (props.letter === "❌") {
+            props.deleteLetter();
+        } else if (props.letter === "✅") {
+            props.nextGuess();
+        } else {
+            props.guessLetter(props.letter);
+        }
+    };
 
-  return (
-    <div className="text-center mx-1 border rounded shadow p-1 bg-white no-highlight">
-      <Ltr
-        className={`mb-0 no-hightlight ${pressed ? "pressed" : ""}`}
-        onMouseLeave={() => mouseLeft()}
-        onClick={() => handleClick()}
-        onMouseDown={() => keyPress()}
-        onMouseUp={() => keyPress()}
-      >
-        {props.letter.toUpperCase()}
-      </Ltr>
-    </div>
-  );
+    return (
+        <div className="text-center mx-1 border rounded shadow p-1 bg-white no-highlight">
+            <Ltr
+                className={`mb-0 no-hightlight ${pressed ? "pressed" : ""}`}
+                onMouseLeave={() => mouseLeft()}
+                onClick={() => handleClick()}
+                onMouseDown={() => keyPress()}
+                onMouseUp={() => keyPress()}
+            >
+                {props.letter.toUpperCase()}
+            </Ltr>
+        </div>
+    );
 }
 
 Letter.propTypes = {
-  letter: PropTypes.string,
-  guessLetter: PropTypes.func,
-  deleteLetter: PropTypes.func,
-  nextGuess: PropTypes.func,
+    letter: PropTypes.string,
+    guessLetter: PropTypes.func,
+    deleteLetter: PropTypes.func,
+    nextGuess: PropTypes.func,
 };
 
 export default Letter;
